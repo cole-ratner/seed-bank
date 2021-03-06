1487b1d31e50eaa065b64271f80c826508568859
Cole Ratner <kolamiti92@gmail.com>
2021-03-06 18:20:52 +0200

    go fmt :)

--- internal/app/drivers/http/rest/presenter/seed.go
+++ internal/app/drivers/http/rest/presenter/seed.go
@@ -6,10 +6,10 @@ import (
 
 //Seed data
 type Seed struct {
-	ID       	  entity.ID `json:"id"`
-	Category      string  	`json:"category"`
-	Name          string 	`json:"name"`
-	Vendor        string 	`json:"vendor"`
-	Quantity      int 		`json:"quantity"`
-	UnitOfMeasure string 	`json:"unitOfMeasure"`
+	ID            entity.ID `json:"id"`
+	Category      string    `json:"category"`
+	Name          string    `json:"name"`
+	Vendor        string    `json:"vendor"`
+	Quantity      int       `json:"quantity"`
+	UnitOfMeasure string    `json:"unitOfMeasure"`
 }
